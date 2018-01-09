f6e828988ef600988912754c22b785696f0686e0
segmentationfaulter <saqib@qubit.com>
2018-01-09 15:02:04 +0500

    pixeling

---
+++ main.js
@@ -0,0 +1,15 @@
+const saleNode = document.getElementById('sale')
+const nonSaleNode = document.getElementById('non-sale')
+
+saleNode.addEventListener('click', sendPixelEvent('sale'), {
+  once: true
+})
+nonSaleNode.addEventListener('click', sendPixelEvent('nonSale'), {
+  once: true
+})
+
+function sendPixelEvent (eventName) {
+  return function () {
+    fbq('trackCustom', eventName)
+  }
+}
