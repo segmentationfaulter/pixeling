89a2cce71ff7cd68e4404e8008992509804bef28
segmentationfaulter <saqib@qubit.com>
2018-01-10 12:00:50 +0500

    change

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ nonSaleNode.addEventListener('click', sendPixelEvent('nonSale'), {
 
 function sendPixelEvent (eventName) {
   return function () {
-    fbq('trackCustom', saleStatus, {
+    fbq('trackCustom', 'saleStatus', {
       sale: this === saleNode ? 1 : 0,
       nonSale: this === nonSaleNode ? 1 : 0
     })
