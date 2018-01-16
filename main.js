aebecb2e788d806a05cb5c73e1e56f61da756b97
segmentationfaulter <saqib@qubit.com>
2018-01-16 12:12:29 +0500

    for prototype

--- main.js
+++ main.js
@@ -1,19 +1,70 @@
-const saleNode = document.getElementById('sale')
-const nonSaleNode = document.getElementById('non-sale')
+const viewNode = document.getElementById('view')
+const purchaseNode = document.getElementById('purchase')
+const wishlistNode = document.getElementById('wishlist')
+const cartNode = document.getElementById('cart')
 
-saleNode.addEventListener('click', sendPixelEvent('sale'), {
+viewNode.addEventListener('click', sendViewEvent, {
   once: true
 })
 
-nonSaleNode.addEventListener('click', sendPixelEvent('nonSale'), {
+purchaseNode.addEventListener('click', sendPurchaseEvent, {
   once: true
 })
 
-function sendPixelEvent (eventName) {
-  return function () {
-    fbq('trackCustom', 'saleStatus', {
-      sale: this === saleNode ? 1 : 0,
-      nonSale: this === nonSaleNode ? 1 : 0
-    })
-  }
+wishlistNode.addEventListener('click', sendWishListEvent, {
+  once: true
+})
+
+cartNode.addEventListener('click', sendCartEvent, {
+  once: true
+})
+
+function sendViewEvent () {
+  window.fbq('track', 'ViewContent', {
+    value: 532,
+    currency: 'USD',
+    contents: {
+      id: 'ABC',
+      quantity: 1,
+      item_price: 600
+    },
+    content_type: 'product'
+  })
+}
+
+function sendPurchaseEvent () {
+  window.fbq('track', 'Purchase', {
+    value: 532,
+    currency: 'USD',
+    contents: {
+      id: 'ABC',
+      quantity: 1,
+      item_price: 600
+    },
+    content_type: 'product'
+  })
+}
+
+function sendWishListEvent () {
+  window.fbq('track', 'AddToWishlist', {
+    value: 532,
+    currency: 'USD',
+    contents: {
+      id: 'ABC',
+      quantity: 1,
+      item_price: 600
+    }
+  })
+}
+
+function sendCartEvent () {
+  window.fbq('track', 'AddToCart', {
+    value: 532,
+    currency: 'USD',
+    contents: {
+      id: 'ABC',
+      quantity: 1,
+      item_price: 600
+    }
+  })
 }
